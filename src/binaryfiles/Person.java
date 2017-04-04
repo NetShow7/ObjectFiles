b4d3785a5bcde2a41222056a6980b0e5738524eb
NetShow7 <netshow.ep@gmail.com>
2017-04-04 12:55:44 +0200

    Revert "Revert "Eliminados constructores en desuso""
    This reverts commit 1fd044998dff44a2c983734d9c7bfb5bfd99f204.

--- src/binaryfiles/Person.java
+++ src/binaryfiles/Person.java
@@ -26,15 +26,6 @@ public class Person implements Serializable {
         setPhone();
     }
 
-    public Person(int d, String n, String s, String a, int p) {
-        dni = d;
-        name = n;
-        surname = s;
-        setBirth();
-        address = a;
-        phone = p;
-    }
-
     public Person(String s) {
 
     }
